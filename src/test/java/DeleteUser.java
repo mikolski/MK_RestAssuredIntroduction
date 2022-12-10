970ecd1b65bb939093494274403dd547ecaecc87
mikolski <mikokrzyz@gmail.com>
2022-12-10 12:10:32 +0100

    10.12 zad 3 done

---
+++ src/test/java/DeleteUser.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteUser extends TestBase{
+    private String users = "/users";
+
+    @Test
+    public void shouldGetFirstUserAndValidateWithPathVariable(){
+        given()
+                .pathParam("userId","1")
+                .when()
+                .delete(BASE_URL+users+"/{userId}")
+                .then()
+                .statusCode(200);
+    }
+}
