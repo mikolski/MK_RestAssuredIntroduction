bbded62defaaccfc911f2c557f5ce1d7c1de85ee
mikolski <mikokrzyz@gmail.com>
2022-12-21 17:29:20 +0100

    Jenkinsfile added

--- src/test/java/advanced/CreateNewPost.java
+++ src/test/java/advanced/CreateNewPost.java
@@ -1,16 +1,20 @@
 package advanced;
 
+import base.TestBase;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import models.Post;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static base.TestBase.BASE_URL;
 import static io.restassured.RestAssured.given;
 
-public class CreateNewPost {
+public class CreateNewPost extends TestBase {
     @Test
     public void ShouldCreateNewUser(){
         Post post = new Post(1,"some title", "some text");
@@ -19,8 +23,8 @@ public class CreateNewPost {
                 .contentType(ContentType.JSON)
                 .when()
                 .post(BASE_URL+"/posts")
-                .then().statusCode(201).
-                extract().response();
+                .then().statusCode(201)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/schemas/postSchema.json")));
 
     }
 }
